cda1ad609f7abe81a05669f520e1918883bbfb57
DM Restoles <dan.restoles@obf.ateneo.edu>
2021-09-04 16:53:36 +0800

    added first person shooting mechanic (gun, damage, target, particle effect on shoot)

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public Camera fpsCamera;
+    public ParticleSystem muzzleFlash;
+    // public ParticleSystem impactEffect;
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            /* GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+             * Destroy(impact, 0.5f);
+             */
+        }
+    }
+}
