fd563d269bb7e53934dd39e2ed25ba1949c52003
DM Restoles <dan.restoles@obf.ateneo.edu>
2021-10-01 15:29:31 +0800

    added main menu navigation to game

---
+++ Assets/Scripts/SelectLevelScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevelScript : MonoBehaviour
+{
+    public void SelectLevelOne()
+    {
+
+    }
+
+    public void SelectLevelTwo()
+    {
+        Debug.Log("Loading scene");
+        SceneManager.LoadScene("DMScene2");
+        Debug.Log("Loading completed");
+    }
+}
