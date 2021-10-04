274644618fd0b21f4631d0fae6c05fa3740732f5
DM Restoles <dan.restoles@obf.ateneo.edu>
2021-10-04 17:12:45 +0800

    Added weapon selection screen
    Weapon selecting works, transferring of weapon to next scene does not work yet

--- Assets/Scripts/SelectLevelScript.cs
+++ Assets/Scripts/SelectLevelScript.cs
@@ -5,15 +5,65 @@ using UnityEngine.SceneManagement;
 
 public class SelectLevelScript : MonoBehaviour
 {
+    public string sceneName;
+    public GameObject playerWeapons;
+    private AsyncOperation sceneAsync;
+
     public void SelectLevelOne()
     {
-
+        sceneName = "tutorial";
     }
 
     public void SelectLevelTwo()
     {
-        Debug.Log("Loading scene");
-        SceneManager.LoadScene("DMScene2");
-        Debug.Log("Loading completed");
+        sceneName = "DMScene2";
+    }
+
+    public void setSceneName(string name)
+    {
+        sceneName = name;
+    }
+
+    public void PlayScene()
+    {
+        StartCoroutine(LoadScene(sceneName));
+    }   
+
+    IEnumerator LoadScene(string name)
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        Debug.Log( currentScene.name );
+        AsyncOperation scene = SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
+        scene.allowSceneActivation = false;
+        sceneAsync = scene;
+
+        while ( scene.progress < 0.9f )
+        {
+            Debug.Log("Loading scene " + " [][] Progress: " + scene.progress);
+            yield return null;
+        }
+        yield return new WaitForSeconds(2.0f);
+        OnFinishedLoadingAllScene();
+    }
+
+    void EnableScene(string name)
+    {
+        sceneAsync.allowSceneActivation = true;
+
+        Scene sceneToLoad = SceneManager.GetSceneByName(name);
+
+        if ( sceneToLoad.IsValid() )
+        {
+            Debug.Log("Scene is valid, loading.");
+            SceneManager.MoveGameObjectToScene(playerWeapons, sceneToLoad);
+            SceneManager.SetActiveScene(sceneToLoad);
+        }
+    }
+
+    void OnFinishedLoadingAllScene()
+    {
+        Debug.Log("Done Loading Scene.");
+        EnableScene(sceneName);
+        Debug.Log("Scene activated!");
     }
 }
---
+++ Assets/Scripts/WeaponSelection.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class WeaponSelection : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public int selectedWeapon = 0;
+    public TMP_Text weaponText;
+    public Image weaponImage;
+
+    void Start()
+    {
+        weapons[selectedWeapon].SetActive(true);
+        weaponText.text = weapons[selectedWeapon].GetComponent<Weapon>().weaponName;
+    }
+
+    public void NextWeapon()
+    {
+        weapons[selectedWeapon].SetActive(false);
+        selectedWeapon = (selectedWeapon + 1) % weapons.Length;
+        weapons[selectedWeapon].SetActive(true);
+        weaponText.text = weapons[selectedWeapon].GetComponent<Weapon>().weaponName;
+    }
+
+    public void PreviousWeapon()
+    {
+        weapons[selectedWeapon].SetActive(false);
+        selectedWeapon--;
+        if (selectedWeapon < 0)
+        {
+            selectedWeapon += weapons.Length;
+        }
+        weapons[selectedWeapon].SetActive(true);
+        weaponText.text = weapons[selectedWeapon].GetComponent<Weapon>().weaponName;
+    }
+}
