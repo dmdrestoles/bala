a63777388e36660d8699ef3d3effb3e5e8814fdf
dmdrestoles <dan.restoles@obf.ateneo.edu>
2022-03-11 14:02:49 +0800

    added password protection for game

---
+++ Assets/Scripts/PasswordCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField passwordField;
+    private string password = "Para sa Katipunan";
+    public GameObject nextScreen;
+
+    void Start()
+    {
+        passwordField.text = "";
+    }
+    public void CheckPasswordIfCorrect()
+    {
+        if (passwordField.text == password)
+        {
+            nextScreen.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
