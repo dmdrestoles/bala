b1d35e8bc4f72f2924681f68f99d93bdacacbbdf
dmdrestoles <dan.restoles@obf.ateneo.edu>
2022-07-14 16:15:05 +0800

    framework for pickups on easter eggs for codex working
    > behavior for pickup to reflect on codex needed
    
    + on pickup, object disappears and logs what the picked up object is

---
+++ Assets/Scripts/PickableObjects/PickableObjectEasterEgg.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableObjectEasterEgg : PickableObjectStateManager
+{
+    public override void HandleObjectPickup(string objectName){
+        if (Input.GetKeyDown(KeyCode.F)){
+            Debug.Log("Picked up object: " + objectName);
+            // Add to codex here
+            Destroy(gameObject);
+        }
+    }
+}
